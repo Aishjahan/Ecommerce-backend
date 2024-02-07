f506c559ca1d5f087ccb63161c69c7e1bccb5181
Aishjahan <aishjahankhan7376@gmail.com>
2024-02-07 20:56:29 +0530

    authentication

--- index.js
+++ index.js
@@ -8,19 +8,66 @@ const userRouter = require("./routes/User");
 const authRouter = require("./routes/Auth");
 const cartRouter = require("./routes/Cart");
 const orderRouter = require("./routes/Order");
-const cors = require('cors');
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+const { User } = require("./model/User");
+
+server.use(
+  session({
+    secret: "keyboard cat",
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+  })
+);
+server.use(passport.authenticate("session"));
 
 server.use(express.json());
-server.use(cors({
-    exposedHeaders:['X-Total-Count']
-}));
-server.use("/products", productRouter.router);
-server.use("/brands" , brandsRouter.router);
-server.use("/categories" , categoriesRouter.router);
+server.use(
+  cors({
+    exposedHeaders: ["X-Total-Count"],
+  })
+);
+server.use("/products", isAuth , productRouter.router);
+server.use("/brands", brandsRouter.router);
+server.use("/categories", categoriesRouter.router);
 server.use("/users", userRouter.router);
 server.use("/auth", authRouter.router);
 server.use("/cart", cartRouter.router);
 server.use("/orders", orderRouter.router);
+
+//passport strategies
+passport.use(
+  new LocalStrategy(async function (username, password, done) {
+    try {
+      const user = await User.findOne({ email: username });
+      if (!user) {
+        done(null, false, { message: "invalid credentails" });
+      } else if (user.password === password) {
+        done(null, user);
+      } else {
+        done(null, false, { message: "invalid credentails" });
+      }
+    } catch (err) {
+      done(err);
+    }
+  })
+);
+//this creates session variables req.user on being called from callbacks
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, {id:user.id, role:user.role});
+  });
+});
+
+//this changes session variable req.user when called from authorized request
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+
 main().catch((err) => console.log(err));
 
 async function main() {
@@ -31,6 +78,14 @@ async function main() {
 server.get("/", (req, res) => {
   res.json({ status: "success" });
 });
+
+function isAuth(req, res, done){
+  if(req.user){
+    done ();
+  }else{
+    res.send(401);
+  }
+}
 
 server.listen(8080, () => {
   console.log("server started");

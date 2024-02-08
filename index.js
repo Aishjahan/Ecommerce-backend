26f130c37e5bc28f33d77ad2dcbec8c0fec118c0
Aishjahan <aishjahankhan7376@gmail.com>
2024-02-08 21:12:10 +0530

    JWT Passport/authentication

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const server = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const crypto = require("crypto");
+const LocalStrategy = require("passport-local").Strategy;
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const jwt = require("jsonwebtoken");
 const productRouter = require("./routes/Product");
 const brandsRouter = require("./routes/Brands");
 const categoriesRouter = require("./routes/Categories");
@@ -8,11 +16,15 @@ const userRouter = require("./routes/User");
 const authRouter = require("./routes/Auth");
 const cartRouter = require("./routes/Cart");
 const orderRouter = require("./routes/Order");
-const cors = require("cors");
-const session = require("express-session");
-const passport = require("passport");
-const LocalStrategy = require('passport-local').Strategy;
 const { User } = require("./model/User");
+const { isAuth, sanitizeUser } = require("./services/common");
+
+const SECRET_KEY = "SECRET_KEY";
+
+//JWT options
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = SECRET_KEY;
 
 server.use(
   session({
@@ -29,35 +41,67 @@ server.use(
     exposedHeaders: ["X-Total-Count"],
   })
 );
-server.use("/products", isAuth , productRouter.router);
-server.use("/brands", brandsRouter.router);
-server.use("/categories", categoriesRouter.router);
-server.use("/users", userRouter.router);
+server.use("/products", isAuth(), productRouter.router);
+server.use("/brands", isAuth(), brandsRouter.router);
+server.use("/categories", isAuth(), categoriesRouter.router);
+server.use("/users", isAuth(), userRouter.router);
 server.use("/auth", authRouter.router);
-server.use("/cart", cartRouter.router);
-server.use("/orders", orderRouter.router);
+server.use("/cart", isAuth(), cartRouter.router);
+server.use("/orders", isAuth(), orderRouter.router);
 
 //passport strategies
 passport.use(
-  new LocalStrategy(async function (username, password, done) {
+  "local",
+  new LocalStrategy(
+    {usernameField : 'email'},
+    async function (email, password, done) {
     try {
-      const user = await User.findOne({ email: username });
+      const user = await User.findOne({ email: email });
       if (!user) {
         done(null, false, { message: "invalid credentails" });
-      } else if (user.password === password) {
-        done(null, user);
-      } else {
-        done(null, false, { message: "invalid credentails" });
       }
+      crypto.pbkdf2(
+        password,
+        user.salt,
+        310000,
+        32,
+        "sha256",
+        async function (err, hashedPassword) {
+          if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
+            return done(null, false, { message: "invalid credentails" });
+          }
+          const token = jwt.sign(sanitizeUser(user), SECRET_KEY);
+          done(null, token);
+        }
+      );
     } catch (err) {
       done(err);
     }
   })
 );
+
+passport.use(
+  "jwt",
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    try {
+      const user = await User.findOne({ id: jwt_payload.sub });
+      if (user) {
+        return done(null, sanitizeUser(user));
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      if (err) {
+        return done(err, false);
+      }
+    }
+  })
+);
+
 //this creates session variables req.user on being called from callbacks
 passport.serializeUser(function (user, cb) {
   process.nextTick(function () {
-    return cb(null, {id:user.id, role:user.role});
+    return cb(null, { id: user.id, role: user.role });
   });
 });
 
@@ -78,14 +122,6 @@ async function main() {
 server.get("/", (req, res) => {
   res.json({ status: "success" });
 });
-
-function isAuth(req, res, done){
-  if(req.user){
-    done ();
-  }else{
-    res.send(401);
-  }
-}
 
 server.listen(8080, () => {
   console.log("server started");
